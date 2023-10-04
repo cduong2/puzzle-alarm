91456e25ffc2ef42285652da0b5e1a01ac3566c8
dchristina2 <104952049+dchristina2@users.noreply.github.com>
2023-10-03 22:13:35 -0400

    preliminary home screen

--- ZooAlarm/app/src/main/java/com/example/zooalarm/MainActivity.java
+++ ZooAlarm/app/src/main/java/com/example/zooalarm/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.zooalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mWeatherButton;
+    private Button mAlarmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWeatherButton = (Button) findViewById(R.id.weather_button);
+        mWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.weather_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mAlarmButton = (Button) findViewById(R.id.alarm_button);
+        mAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.alarm_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
