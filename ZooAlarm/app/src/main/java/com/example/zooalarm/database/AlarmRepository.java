d47252c5081ef72b82be10c4ee34e5507107e0f5
dchristina2 <104952049+dchristina2@users.noreply.github.com>
2023-10-20 13:00:00 -0400

    added view model

--- ZooAlarm/app/src/main/java/com/example/zooalarm/database/AlarmRepository.java
+++ ZooAlarm/app/src/main/java/com/example/zooalarm/database/AlarmRepository.java
@@ -6,7 +6,7 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
-class AlarmRepository {
+public class AlarmRepository {
 
     private AlarmDao mAlarmDao;
     private LiveData<List<Alarm>> mAllAlarms;
@@ -15,7 +15,7 @@ class AlarmRepository {
     // dependency. This adds complexity and much more code, and this sample is not about testing.
     // See the BasicSample in the android-architecture-components repository at
     // https://github.com/googlesamples
-    AlarmRepository(Application application) {
+    public AlarmRepository(Application application) {
         AlarmRoomDatabase db = AlarmRoomDatabase.getDatabase(application);
         mAlarmDao = db.alarmDao();
         mAllAlarms = mAlarmDao.getAlarms();
@@ -23,13 +23,13 @@ class AlarmRepository {
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
-    LiveData<List<Alarm>> getAllAlarms() {
+    public LiveData<List<Alarm>> getAllAlarms() {
         return mAllAlarms;
     }
 
     // You must call this on a non-UI thread or your app will throw an exception. Room ensures
     // that you're not doing any long running operations on the main thread, blocking the UI.
-    void insert(Alarm alarm) {
+    public void insert(Alarm alarm) {
         AlarmRoomDatabase.databaseWriteExecutor.execute(() -> {
             mAlarmDao.insert(alarm);
         });
